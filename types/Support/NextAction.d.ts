41fc17a18ca0b8414f41308133e809f8139b4a09
Donald Pakkies <donaldpakkies@gmail.com>
2021-09-27 17:49:04 +0200

    build

---
+++ types/Support/NextAction.d.ts
@@ -0,0 +1,10 @@
+export default class NextAction {
+    /**
+    @param {Function} callback
+    */
+    constructor(callback: Function);
+    callback: Function;
+    [Ψ__init__]($$?: any): void;
+}
+declare const Ψ__init__: unique symbol;
+export {};
---
+++ types/index.d.ts
@@ -0,0 +1,5 @@
+import next from "./Support/Helpers/next";
+import NextAction from "./Support/NextAction";
+import NextjsServiceResolver from "./NextjsServiceResolver";
+import NextService from "./Support/NextService";
+export { next, NextAction, NextjsServiceResolver, NextService };
