6133e7715435f96134bcd1a8268fa771600f20c9
Donald Pakkies <donaldpakkies@gmail.com>
2021-09-28 20:10:39 +0200

    build plugin

---
+++ types/Exceptions/NextjsBuildMissingException.d.ts
@@ -0,0 +1,3 @@
+export default class NextjsBuildMissingException extends ApplicationException {
+}
+import { ApplicationException } from "@formidablejs/framework";
--- types/Support/NextAction.d.ts
+++ types/Support/NextAction.d.ts
@@ -1,9 +1,9 @@
 export default class NextAction {
     /**
-    @param {Function} callback
+    @param {Function|[Function, string]} callback
     */
-    constructor(callback: Function);
-    callback: Function;
+    constructor(callback: Function | [Function, string]);
+    callback: Function | [Function, string];
     [Ψ__init__]($$?: any): void;
 }
 declare const Ψ__init__: unique symbol;
